dcbe67fbcd919356cba440b693a2afcdb8ed052a
JustChad1 <ramosrichard176@gmail.com>
2024-11-04 18:41:32 +0800

    initial commit

---
+++ favoriteNumber.js
@@ -0,0 +1,16 @@
+function favNum() {
+let favNumber = Math.floor(Math.random() * 20) + 1; 
+
+let guess;
+while (guess !== favNumber) {
+    guess = parseInt(prompt("Guess the favorite number."));
+
+    if (guess < favNumber) {
+        alert("Too low, try again.");
+    } else if (guess > favNumber){
+        alert("Too high, try again.")
+    } else {
+        alert("Correct, you guessed the favorite number.");
+        break;
+    }
+}}
